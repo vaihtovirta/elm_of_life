aacab01c7523c0c647ee58c03034520ba7cdad55
Emil Shakirov <me+github@emil.sh>
2019-01-06 23:49:20 +0100

    Refactor Cell module

--- src/Cell.elm
+++ src/Cell.elm
@@ -1,45 +1,47 @@
-module Cell exposing (..)
+module Cell exposing (buildCellList)
 
 import Array exposing (fromList)
+import Matrix exposing (Matrix)
 import Svg exposing (rect)
 import Svg.Attributes exposing (fill, height, stroke, viewBox, width, x, y)
 import Svg.Events exposing (onClick)
-import Matrix exposing (Matrix)
+
 
 cellSize =
     15
 
+
+cellSizeString =
+    "15"
+
+
+rectPosition value =
+    String.fromInt (value * cellSize)
+
+
+fillColor alive =
+    if alive then
+        "green"
+
+    else
+        "white"
+
+
 cell item onClickHanlder =
-    let
-        xString =
-            String.fromInt (item.x * cellSize)
-
-        yString =
-            String.fromInt (item.y * cellSize)
-
-        cellSizeString =
-            String.fromInt cellSize
-
-        fillColor =
-            if item.alive then
-                "green"
-
-            else
-                "white"
-    in
     rect
-        [ x xString
-        , y yString
+        [ rectPosition item.x |> x
+        , rectPosition item.y |> y
         , width cellSizeString
         , height cellSizeString
-        , fill fillColor
+        , item.alive |> fillColor |> fill
         , stroke "black"
         , onClick onClickHanlder
         ]
         []
 
+
 buildCellList generation onClickHanlder =
     generation
-    |> Matrix.toArray
-    |> Array.toList
-    |> List.map(\item -> cell item (onClickHanlder item.id))
+        |> Matrix.toArray
+        |> Array.toList
+        |> List.map (\item -> cell item (onClickHanlder item.id))
