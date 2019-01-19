b8d0ba598bbbc8f5ec968b014bfb53b2149194b8
Emil Shakirov <me+github@emil.sh>
2019-01-19 13:58:05 +0100

    Introduce alive cells memoization between generations (#1)
    * Introduce alive cells memoization between generations
    
    * Add minified js file

--- src/MatrixUtils.elm
+++ src/MatrixUtils.elm
@@ -1,41 +1,55 @@
-module MatrixUtils exposing (fieldMatrix, randomFieldMatrix, isAlive)
+module MatrixUtils exposing (cellNeighbours, detectFate, fieldMatrix, getAliveCells, isAlive, randomFieldMatrix)
 
 import Array exposing (Array)
 import Dict exposing (Dict)
 import Matrix exposing (Matrix)
-import Neighbours exposing (MatrixTopology(..), neighbours)
-import Types exposing(Cell)
-import String exposing (fromInt)
 import Msgs exposing (Msg(..))
+import Neighbours exposing (MatrixTopology(..), neighbours)
+import String exposing (fromInt)
+import Types exposing (Cell)
 
-type alias Pattern = List(List Int)
+
+type alias Pattern =
+    List (List Int)
+
+
+sideSize : Int
+sideSize =
+    50
+
 
 glider : Pattern
 glider =
     [ [ 18, 22 ], [ 19, 22 ], [ 20, 22 ], [ 20, 21 ], [ 19, 20 ] ]
 
+
 blinkers : Pattern
 blinkers =
-    [ [ 19, 20 ], [18, 21], [ 19, 21 ], [20, 21], [19, 22] ]
+    [ [ 19, 20 ], [ 18, 21 ], [ 19, 21 ], [ 20, 21 ], [ 19, 22 ] ]
+
 
 patterns : Dict String Pattern
 patterns =
-  Dict.fromList
-    [ ("glider", glider)
-    , ("blinkers", blinkers)
-    ]
+    Dict.fromList
+        [ ( "glider", glider )
+        , ( "blinkers", blinkers )
+        ]
 
 
 getPattern : String -> Pattern
 getPattern patternName =
     case patterns |> Dict.get patternName of
-        Just list -> list
-        Nothing -> []
+        Just list ->
+            list
+
+        Nothing ->
+            []
 
 
 fieldMatrix : String -> Matrix Cell
 fieldMatrix patternName =
-    Matrix.generate 50 50
+    Matrix.generate sideSize
+        sideSize
         (\x y ->
             let
                 alive =
@@ -47,32 +61,38 @@ fieldMatrix patternName =
 
 randomFieldMatrix : Array Bool -> Matrix Cell
 randomFieldMatrix seed =
-    Matrix.generate 50 50
+    Matrix.generate sideSize sideSize
         (\x y -> Cell x y (cellId x y) False)
-    |> Matrix.indexedMap(\x y cell ->
-        let
-            seedIndex = x + y * 50
-            alive = case Array.get(seedIndex) seed of
-                Just value -> value
-                Nothing -> False
-        in
-        Cell x y (cellId x y) alive
-    )
+        |> Matrix.indexedMap
+            (\x y cell ->
+                let
+                    seedIndex =
+                        x + y * sideSize
+
+                    alive =
+                        case Array.get seedIndex seed of
+                            Just value ->
+                                value
+
+                            Nothing ->
+                                False
+                in
+                Cell x y (cellId x y) alive
+            )
 
 
 cellId x y =
     fromInt x ++ "-" ++ fromInt y
 
 
-cellNeighbours : Matrix Cell -> Cell -> Array.Array Cell
+cellNeighbours : Matrix Cell -> Cell -> List Cell
 cellNeighbours matrix { posX, posY } =
-    neighbours Torus posX posY matrix
+    neighbours Torus posX posY matrix |> Array.toList
 
 
 liveNeighboursCount : Matrix Cell -> Cell -> Int
 liveNeighboursCount matrix cell =
     cellNeighbours matrix cell
-        |> Array.toList
         |> List.filter .alive
         |> List.length
 
@@ -101,26 +121,21 @@ isAlive matrix cell =
     in
     case cell.alive of
         True ->
-            case count of
-                0 ->
-                    False
+            if count == 2 || count == 3 then
+                True
 
-                1 ->
-                    False
-
-                2 ->
-                    True
-
-                3 ->
-                    True
-
-                _ ->
-                    False
+            else
+                False
 
         False ->
-            case count of
-                3 ->
-                    True
+            count == 3
 
-                _ ->
-                    False
+
+getAliveCells : Matrix Cell -> List Cell
+getAliveCells matrix =
+    matrix |> Matrix.toArray |> Array.filter .alive |> Array.toList
+
+
+detectFate : Matrix Cell -> Cell -> Cell
+detectFate matrix cell =
+    { cell | alive = isAlive matrix cell }
